9fb062270a06bae49bef101f20cf272eb4ef8d41
Sergey V. Kior <svkior@gmail.com>
2014-08-22 16:59:43 +0400

    working curses menu

--- build_sw24x5forDocker.sh
+++ build_sw24x5forDocker.sh
@@ -6,9 +6,21 @@ cd ./distout/docker
 sudo docker build -t svkior/switcher24x7 .
 CID=$(sudo docker run -d svkior/switcher24x7)
 
+cd ../..
+
+echo sudo docker logs -f $CID >current_log.sh
+chmod +x current_log.sh
+
+echo sudo docker stop $CID >current_stop.sh
+chmod +x current_stop.sh
+
 echo Deployed with CID $CID
 echo Press enter to quit
 read
 
-sudo docker stop $CID
+./current_stop.sh
+
+rm current_log.sh
+rm current_stop.sh
+
 
--- dockergui.go
+++ dockergui.go
@@ -1,7 +1,79 @@
 package main
 
-import "fmt"
+import (
+	"code.google.com/p/goncurses"
+	//	"fmt"
+	"log"
+)
+
+const (
+	WEIGHT = 10
+	WIDTH  = 30
+)
 
 func main() {
-	fmt.Println("Hello, World")
+	stdscr, err := goncurses.Init()
+	if err != nil {
+		log.Fatal("init:", err)
+	}
+	defer goncurses.End()
+
+	goncurses.Raw(true)
+	goncurses.Echo(false)
+	goncurses.Cursor(0)
+	stdscr.Clear()
+	stdscr.Keypad(true)
+
+	menu_items := []string{
+		"Add controller [ ]",
+		"Add Switcher [ ]",
+	}
+
+	items := make([]*goncurses.MenuItem, len(menu_items))
+	for i, val := range menu_items {
+		items[i], _ = goncurses.NewItem(val, "")
+		defer items[i].Free()
+	}
+
+	menu, err := goncurses.NewMenu(items)
+	if err != nil {
+		stdscr.Print(err)
+		return
+	}
+
+	defer menu.Free()
+
+	menu.Option(goncurses.O_ONEVALUE, false)
+
+	y, _ := stdscr.MaxYX()
+	stdscr.MovePrint(y-3, 0, "UpDown to move, spacebar to toggle, enter: log. q to exit")
+
+	stdscr.Refresh()
+	menu.Post()
+	defer menu.UnPost()
+
+	for {
+		goncurses.Update()
+		ch := stdscr.GetChar()
+
+		switch ch {
+		case 'q':
+			return
+		case ' ':
+			menu.Driver(goncurses.REQ_TOGGLE)
+		case goncurses.KEY_RETURN, goncurses.KEY_ENTER:
+			var list string
+			for _, item := range menu.Items() {
+				if item.Value() {
+					list += "\"" + item.Name() + "\""
+				}
+			}
+			stdscr.Move(20, 0)
+			stdscr.ClearToEOL()
+			stdscr.MovePrint(20, 0, list)
+			stdscr.Refresh()
+		default:
+			menu.Driver(goncurses.DriverActions[ch])
+		}
+	}
 }
