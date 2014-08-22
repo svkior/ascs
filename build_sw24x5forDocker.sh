b5a65ebcccf714c760194f7f1709ed20829bd33e
Sergey V. Kior <svkior@gmail.com>
2014-08-22 10:53:54 +0400

    test2

--- build_sw24x5forDocker.sh
+++ build_sw24x5forDocker.sh
@@ -1,6 +1,6 @@
 mkdir -p ./distout/docker
 
-go build  -o ./distout/docker/main_cli main_cli.go
+go build  -o ./distout/docker/work/main_cli main_cli.go
 cp ./conf/lnx_switch24_docker.json ./distout/docker/
 cd ./distout/docker
 sudo docker build -t svkior/switcher24x7 .
